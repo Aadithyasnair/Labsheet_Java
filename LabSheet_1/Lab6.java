060031e3be8ee1763a8806922920e44b47808bc7
Aadithya S Nair <145871282+Darthcoder2006@users.noreply.github.com>
2025-01-29 09:57:56 +0530

    Add files via upload

---
+++ LabSheet_1/Lab6.java
@@ -0,0 +1,18 @@
+package labsheet;
+import java.util.Scanner;
+
+public class Lab6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double accountBalance = 15000.00;
+        System.out.println("Enter the withdrawal amount: ");
+        double withdrawalAmount = scanner.nextDouble();
+        if (withdrawalAmount <= accountBalance) {
+            accountBalance -= withdrawalAmount;
+            System.out.println("Withdrawal successful! New balance: " + accountBalance);
+        } else {
+            System.out.println("Insufficient funds. Please check your balance.");
+        }
+        scanner.close();
+    }
+}
